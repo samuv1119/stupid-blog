e3f8fd19acd2fa165b9f38b898edf5b0f4d4f0b5
samuv <samuv1119@gmail.com>
2025-08-04 10:42:11 +0800

    feat:加入了状态设置api和测试显示界面

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 api_key=os.getenv("API_KEY")
 
 counter=0
+status="待设置"
 
 @app.route('/')
 def hello_world():
-    return "Hello World! - by samuv."
+    return f"Hello World! - by samuv.\n\r\n\rsamuv 现在的状态是:{status}"
 
 @app.route('/test')
 def show_counter():
@@ -27,6 +28,16 @@ def api_test():
     counter+=1
     return "success 2 increase counter."
 
+@app.route("/status_set",methods=['post'])
+def api_status_set():
+    provided_key=request.headers.get("X-API-KEY","")
+    if provided_key != api_key:
+        return "fail 2 match key."
+    global status
+    status=request.args.get("status","待设置")
+    return f"success 2 set status 2 {status}"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
