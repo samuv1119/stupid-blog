51a36aee6983d9b97c04b3b6206e516857fba50f
samuv <samuv@qq.com>
2025-08-05 11:07:35 +0800

    feat:加入了美观的主页和tinydb持久化存储

--- app.py
+++ app.py
@@ -1,43 +1,63 @@
-from flask import Flask,request
-from dotenv import load_dotenv 
+import sys
+import time
+
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
 import os
+import model
+
 
 load_dotenv()
 
 app = Flask(__name__)
 
-api_key=os.getenv("API_KEY")
+api_key = os.getenv("API_KEY")
 
-counter=0
-status="待设置"
+status_data=model.StatusDataMgr()
+
+if len(status_data)<1:
+    status_data.append_status(status_data.create_status("初始记录","这是第一条状态记录"))
+
+
+# todo: 加入tinydb数据库来存储状态,历史状态,感想等信息
 
 @app.route('/')
 def hello_world():
-    return f"Hello World! - by samuv.\n\r\n\rsamuv 现在的状态是:{status}"
+    return f"Hello World! - by samuv."
+
 
 @app.route('/test')
-def show_counter():
-    return f"the counter has touched {counter} times."
+def test():
+    item=status_data.create_status("test"+str(int(time.time()%100)),"des"+str(int(time.time()%1000)))
+    status_data.append_status(item)
+    return f"item append success"
 
-@app.route("/cunter_set",methods=['post'])
+
+@app.route("/cunter_set", methods=['post'])
 def api_test():
-    provided_key=request.headers.get("X-API-KEY","")
+    provided_key = request.headers.get("X-API-KEY", "")
     if provided_key != api_key:
         return "fail 2 match key."
     global counter
-    counter+=1
+    counter += 1
     return "success 2 increase counter."
 
-@app.route("/status_set",methods=['post'])
+
+@app.route("/status_set", methods=['post'])
 def api_status_set():
-    provided_key=request.headers.get("X-API-KEY","")
+    provided_key = request.headers.get("X-API-KEY", "")
     if provided_key != api_key:
         return "fail 2 match key."
-    global status
-    status=request.args.get("status","待设置")
-    return f"success 2 set status 2 {status}"
+    present_status = request.args.get("status", "待设置")
+    return f"success 2 set status 2 {present_status}"
+
+
+@app.route("/test_index")
+def test_index():
+    present_status = status_data.get_present_status()
+    history_status = status_data.get_history_status()
+    return render_template("status-ds-4.html",present=present_status,history=history_status)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
---
+++ model.py
@@ -0,0 +1,53 @@
+from tinydb import TinyDB, Query
+import time
+
+from datetime import datetime
+
+
+db = TinyDB("db.json")
+
+class StatusDataMgr:
+    def __init__(self):
+        self.table = db.table("status_history")
+        self.query = Query()
+
+    def __len__(self):
+        return len(self.table)
+
+    def create_status(self, status,description):
+        now=time.time()
+        status_data= {"start_time":now, "end_time":-1, "status":status, "description":description}
+        st=datetime.fromtimestamp(now).strftime("%y-%m-%d %H:%M")
+        status_data["start_time_str"] =st
+        status_data["end_time_str"] ="现在"
+        status_data["index"]=self.get_max_index()+1
+        return status_data
+
+    def append_status(self, status: dict):
+        now=status["start_time"]-1
+        self.touch_status(now)
+        self.table.insert(status)
+
+    def get_max_index(self):
+        docs=self.table.all()
+        mx=0
+        for doc in docs:
+            mx=max(mx,doc["index"])
+        return mx
+
+
+    def touch_status(self,timestamp):
+        if len(self.table.all())>0:
+            last=self.table.get(self.query.index==self.get_max_index())
+            last["end_time"]=timestamp
+            st=datetime.fromtimestamp(timestamp).strftime("%y-%m-%d %H:%M")
+            last["end_time_str"]=st
+            self.table.update(last,self.query.index==self.get_max_index())
+
+    def get_present_status(self):
+        return self.table.get(self.query.index == self.get_max_index())
+
+    def get_history_status(self):
+        docs=self.table.search(self.query.index < self.get_max_index())
+        sorted_docs=sorted(docs, key=lambda doc: doc["index"], reverse=True)
+        return sorted_docs
